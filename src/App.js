7ab624a44e0b112d1d0f1900f8dfb758ab4b7a9d
walidMahjoub <walid.kouissah.mahjoub@gmail.com>
2022-05-22 01:07:57 +0200

    signup (#3)
    Co-authored-by: walid mahjoub <wkouissahmahjoub@thefork.com>

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import React from "react";
 import Home from "./pages/Home";
 import Signin from "./pages/Signin";
-import Signup from "./pages/Signup";
 import Header from "./Header";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import SignupCandidate from "./pages/Signup/Candidate";
+import SignupCampany from "./pages/Signup/Campany";
 
 const App = () => {
   return (
     <Router>
       <Header />
-
-      {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-      <Routes>
-        <Route path="signin" element={<Signin />} />
-        <Route path="Signup" element={<Signup />} />
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <div style={{ padding: 30 }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="signin" element={<Signin />} />
+          <Route path="signup">
+            <Route path="candidate" element={<SignupCandidate />} />
+            <Route path="campany" element={<SignupCampany />} />
+          </Route>
+        </Routes>
+      </div>
     </Router>
   );
 };
